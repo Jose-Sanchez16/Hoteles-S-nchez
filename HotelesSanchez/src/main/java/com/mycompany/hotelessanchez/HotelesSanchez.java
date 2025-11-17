5484ea75f7ee72bbe0aff35b110ac188d8be2065
Jose-Sanchez16 <jose.sanchez.moreno.al@iespoligonosur.org>
2025-11-17 18:40:20 +0100

    Creacion proyecto

---
+++ HotelesSanchez/src/main/java/com/mycompany/hotelessanchez/HotelesSanchez.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hotelessanchez;
+
+/**
+ *
+ * @author alumnadotarde
+ */
+public class HotelesSanchez {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
